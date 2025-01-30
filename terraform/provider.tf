9107c4a96d7efe65d7b159746207ea09361ba229
Yah-Mo1 <yahyamohamed1231@outlook.com>
2025-01-30 01:42:46 +0000

    set up workflow pipelines

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,13 +5,19 @@ terraform {
       version = "5.83.1"
     }
   }
+
+   backend "s3" {
+    bucket                  = "statefile-backend-s3-bucket"
+    key                     = "threatModel/terraform.tfstate" //needs changing in cicd
+    region                  = "us-east-1"
+  }
 }
 
 
 // use aws configure to log into aws cli 
 provider "aws" {
   # Configuration options
-  region     = var.region
+  region     = "us-east-1"
   access_key = ""
   secret_key = ""
 }
