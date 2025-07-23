f9e7d20e0cb41b1e7b6042fd5c833339c945100d
Yah-Mo1 <yahyamohamed1231@outlook.com>
2025-07-23 14:29:09 +0100

    Modify autoscaling module

--- terraform/modules/autoscaling/autoscaling.tf
+++ terraform/modules/autoscaling/autoscaling.tf
@@ -28,23 +28,23 @@ resource "aws_appautoscaling_policy" "ecs_cpu" {
 }
 
 // Scale In Policy
-resource "aws_appautoscaling_policy" "scale_in" {
-  name               = "${var.ecs_cluster_name}-scale-in"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+# resource "aws_appautoscaling_policy" "scale_in" {
+#   name               = "${var.ecs_cluster_name}-scale-in"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
 
-  target_tracking_scaling_policy_configuration {
-    target_value       = 20
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 300
+#   target_tracking_scaling_policy_configuration {
+#     target_value       = 20
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 300
 
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-  }
-}
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#   }
+# }
 
 
 
